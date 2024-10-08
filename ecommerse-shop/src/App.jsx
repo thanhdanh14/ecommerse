10885bcb3832d5fb0c5612d3922d26d0614abf37
Danh Bien <danh.bien@buymed.com>
2024-10-08 17:28:04 +0700

    add banner

--- ecommerse-shop/src/App.jsx
+++ ecommerse-shop/src/App.jsx
@@ -1,14 +1,18 @@
+import Banner from '@components/Banner/Banner';
 import Footer from '@components/Footer/Footer';
 import Header from '@components/Header/Header';
+import HomePage from '@components/Home/Home';
 import MainLayout from '@components/Layout/Layout';
 
 function App() {
     return (
         <div>
-            <MainLayout>
-                <Header />
-                {/* <Footer /> */}
-            </MainLayout>
+            {/* <MainLayout> */}
+            {/* <Header />
+            <Banner /> */}
+            {/* <Footer /> */}
+            {/* </MainLayout> */}
+            <HomePage />
         </div>
     );
 }
---
+++ ecommerse-shop/src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import Button from '@components/Button/Button';
+import styles from './styles.module.scss';
+function Banner() {
+    const { container, content, title, des, btn } = styles;
+    return (
+        <div className={container}>
+            <div className={content}>
+                <h1 className={title}>XStore Marseille04 Demo</h1>
+                <div className={des}>
+                    Make yours celebrations even more special this years with
+                    beautiful.
+                </div>
+                <Button content='Go to shop' />
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
